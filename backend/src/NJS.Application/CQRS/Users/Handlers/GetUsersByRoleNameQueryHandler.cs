98759c27a660f0516d5d26f10d8f549f761c0b5a
Arun <mali.arun44@gmail.com>
2025-02-23 21:52:30 +0530

    Revrt GetUsersByRoleNameQueryHandler

--- backend/src/NJS.Application/CQRS/Users/Handlers/GetUsersByRoleNameQueryHandler.cs
+++ backend/src/NJS.Application/CQRS/Users/Handlers/GetUsersByRoleNameQueryHandler.cs
@@ -1,75 +1,48 @@
-using System.Collections.Generic;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NJS.Application.Dtos;
+using NJS.Application.CQRS.Users.Queries;
+using NJS.Domain.Entities;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
-using Microsoft.AspNetCore.Identity;
-using NJS.Application.CQRS.Users.Queries;
-using NJS.Application.Dtos;
-using NJS.Domain.Entities;
 
 namespace NJS.Application.CQRS.Users.Handlers
 {
-    public class GetUsersByRoleNameQueryHandler : IRequestHandler<GetUsersByRoleNameQuery, IEnumerable<UserDto>>
+    public class GetUsersByRoleNameQueryHandler : IRequestHandler<GetUsersByRoleNameQuery, List<UserDto>>
     {
         private readonly UserManager<User> _userManager;
         private readonly RoleManager<Role> _roleManager;
-        private readonly IMediator _mediator;
 
-        public GetUsersByRoleNameQueryHandler(
-            UserManager<User> userManager,
-            RoleManager<Role> roleManager,
-            IMediator mediator)
+        public GetUsersByRoleNameQueryHandler(UserManager<User> userManager, RoleManager<Role> roleManager)
         {
             _userManager = userManager;
             _roleManager = roleManager;
-            _mediator = mediator;
         }
 
-        public async Task<IEnumerable<UserDto>> Handle(GetUsersByRoleNameQuery request, CancellationToken cancellationToken)
+        public async Task<List<UserDto>> Handle(GetUsersByRoleNameQuery request, CancellationToken cancellationToken)
         {
+            var role = await _roleManager.FindByNameAsync(request.RoleName);
+            if (role == null)
+                return new List<UserDto>();
+
             var usersInRole = await _userManager.GetUsersInRoleAsync(request.RoleName);
 
-            var userDtos = new List<UserDto>();
-            foreach (var user in usersInRole)
+            return usersInRole.Select(user => new UserDto
             {
-                var roles = await _userManager.GetRolesAsync(user);
-                var permissions = new List<string>();
-
-                var roleDto = new List<RoleDto>();
-                foreach (var roleName in roles)
-                {
-                    var role = await _roleManager.FindByNameAsync(roleName);
-
-                    if (role != null)
-                    {
-                        roleDto.Add(new RoleDto
-                        {
-                            Name = role.Name,
-                            Id = role.Id
-                        });
-                        var rolePermissions = await _mediator.Send(new GetRolePermissionsQuery(role.Id));
-                        permissions.AddRange(rolePermissions.Select(p => p.Name));
-                    }
-                }
-
-                userDtos.Add(new UserDto
-                {
-                    Id = user.Id,
-                    UserName = user.UserName,
-                    Name = user.Name,
-                    Email = user.Email,
-                    StandardRate = user.StandardRate ?? 0,
-                    IsConsultant = user.IsConsultant,
-                    Avatar = user.Avatar,
-                    Roles = roleDto,
-                    Permissions = permissions.Distinct().ToList(),
-                    CreatedAt = user.CreatedAt,
-                    UpdatedAt = user.LastLogin.Value
-                });
-            }
-
-            return userDtos;
+                Id = user.Id,
+                UserName = user.UserName,
+                Name = user.Name,
+                Email = user.Email,
+                CreatedAt = user.CreatedAt,
+                UpdatedAt = user.CreatedAt, // No UpdatedAt in User entity
+                StandardRate = user.StandardRate ?? 0m,
+                IsConsultant = user.IsConsultant,
+                Avatar = user.Avatar ?? string.Empty,
+                Roles = new List<RoleDto>(), // Roles mapping needs to be implemented separately
+                Permissions = new List<string>() // Permissions retrieval needs to be implemented
+            }).ToList();
         }
     }
 }
