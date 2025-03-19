3c0757f727ad87f48333e27962627dbde96087bc
harshalHrs <156283824+harshalHrs@users.noreply.github.com>
2025-03-19 13:18:50 +0530

    Add  the  Missing Property from frontend to backend

--- backend/src/NJS.Domain/Entities/Project.cs
+++ backend/src/NJS.Domain/Entities/Project.cs
@@ -14,6 +14,7 @@ namespace NJS.Domain.Entities
         [Required]
         [StringLength(100)]
         public string? Name { get; set; }
+        public int ProjectNo { get; set; } 
 
         [Required]
         [StringLength(100)]
@@ -23,22 +24,48 @@ namespace NJS.Domain.Entities
         [StringLength(50)]
         public string? TypeOfClient { get; set; }
 
+		[ForeignKey("ProjectManager")]
+		public string? ProjectManagerId { get; set; }
+		public virtual User ProjectManager { get; set; }
+
+		[ForeignKey("SeniorProjectManager")]
+		public string? SeniorProjectManagerId { get; set; }
+		public virtual User SeniorProjectManager { get; set; }
+
+		[ForeignKey("RegionalManager")]
+		public string? RegionalManagerId { get; set; }
+		public virtual User RegionalManager { get; set; }
+
+        public string Office {  get; set; }
+		public string Region { get; set; }
+        public string TypeOfJob { get; set; }
+
         [Required]
         [StringLength(50)]
         public string? Sector { get; set; }
-        public string Region { get; set; }
         public string FeeType { get; set; }
+
 
         [Column(TypeName = "decimal(18,2)")]
         public decimal EstimatedCost { get; set; }
 
-        [Column(TypeName = "decimal(18,2)")]
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Budget {  get; set; }
+
+        public string Priority {  get; set; }
+
+		[Required]
+		[StringLength(3)]
+		public string? Currency { get; set; }
+		public DateTime? StartDate { get; set; }
+
+		public DateTime? EndDate { get; set; }
+
+
+
+		[Column(TypeName = "decimal(18,2)")]
         public decimal? CapitalValue { get; set; }
-
-        public DateTime? StartDate { get; set; }
-
-        public DateTime? EndDate { get; set; }
-
+        
         [Required]
         public ProjectStatus Status { get; set; }
 
@@ -52,23 +79,7 @@ namespace NJS.Domain.Entities
         
         [StringLength(50)]
         public string? ContractType { get; set; }
-
-        [Required]
-        [StringLength(3)]
-        public string? Currency { get; set; }
-
-        [ForeignKey("ProjectManager")]
-        public string? ProjectManagerId { get; set; }
-        public virtual User ProjectManager { get; set; }
-
-        [ForeignKey("RegionalManager")]
-        public string? RegionalManagerId { get; set; }
-        public virtual User RegionalManager { get; set; }
-
-        [ForeignKey("SeniorProjectManager")]
-        public string? SeniorProjectManagerId { get; set; }
-        public virtual User SeniorProjectManager { get; set; }
-
+        
         [ForeignKey("OpportunityTrackingId")]
         public int? OpportunityTrackingId { get; set; }
         public virtual OpportunityTracking OpportunityTracking { get; set; }
