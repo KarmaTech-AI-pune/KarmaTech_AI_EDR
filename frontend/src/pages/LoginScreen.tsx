af7e4e4946760e4fb1391306b068bfa8874064d2
Shekhar Iyer <shekhareagle@gmail.com>
2025-03-28 23:43:17 +0530

    New patch version for deploy

--- frontend/src/pages/LoginScreen.tsx
+++ frontend/src/pages/LoginScreen.tsx
@@ -89,7 +89,7 @@ export const LoginScreen: React.FC = () => {
                         color: '#666'
                     }}
                 >
-                    Version 1.3.0
+                    Version 1.3.1
 
                 </Typography>
             </Container>
