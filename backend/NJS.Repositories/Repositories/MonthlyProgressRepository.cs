fe4abe8701557d87e4aed7e4fd73e394d75f7f66
harshalHrs <156283824+harshalHrs@users.noreply.github.com>
2025-07-03 12:39:07 +0530

    Create a new API to update the manpower planning data in the Monthly Progress module.

--- backend/NJS.Repositories/Repositories/MonthlyProgressRepository.cs
+++ backend/NJS.Repositories/Repositories/MonthlyProgressRepository.cs
@@ -102,5 +102,11 @@ namespace NJS.Repositories.Repositories
                                      .ThenInclude(bt => bt.PercentCompleteOnCosts)
                                  .ToListAsync();
         }
+
+        public async Task UpdateManpowerPlanningAsync(ManpowerPlanning manpowerPlanning)
+        {
+            _context.ManpowerPlannings.Update(manpowerPlanning);
+            await _context.SaveChangesAsync();
+        }
     }
 }
