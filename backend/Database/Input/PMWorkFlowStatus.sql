1ccd8a50514a6390a2197537b54e7504c03225ee
harshalHrs <156283824+harshalHrs@users.noreply.github.com>
2025-09-16 16:31:00 +0530

    Pass the tenant ID in the PM workflow script.

--- backend/Database/Input/PMWorkFlowStatus.sql
+++ backend/Database/Input/PMWorkFlowStatus.sql
@@ -1,14 +1,23 @@
 IF NOT EXISTS (SELECT TOP 1 1 FROM PMWorkflowStatuses)
 BEGIN
+    DECLARE @TenantId INT = 1; -- Default TenantId
+	DECLARE @TenantIdParam INT = NULL; -- Optional TenantId parameter
+
+    -- Optional TenantId parameter
+	IF @TenantIdParam IS NOT NULL
+	BEGIN
+		SET @TenantId = @TenantIdParam;
+	END
+
     SET IDENTITY_INSERT PMWorkflowStatuses ON;
-    INSERT INTO PMWorkflowStatuses (Id, Status)
+    INSERT INTO PMWorkflowStatuses (Id, Status, TenantId)
     VALUES
-    (1, 'Initial'),
-    (2, 'Sent for Review'),
-    (3, 'Review Changes'),
-    (4, 'Sent for Approval'),
-    (5, 'Approval Changes'),
-    (6, 'Approved');
+    (1, 'Initial', @TenantId),
+    (2, 'Sent for Review', @TenantId),
+    (3, 'Review Changes', @TenantId),
+    (4, 'Sent for Approval', @TenantId),
+    (5, 'Approval Changes', @TenantId),
+    (6, 'Approved', @TenantId);
     SET IDENTITY_INSERT PMWorkflowStatuses OFF;
     PRINT 'PMWorkflowStatuses data inserted successfully';
 END
