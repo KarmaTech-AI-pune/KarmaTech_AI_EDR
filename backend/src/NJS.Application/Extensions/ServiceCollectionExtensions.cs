630959adb9eb8c2eb5af443042fe985c9b10ba09
harshalHrs <156283824+harshalHrs@users.noreply.github.com>
2025-10-01 10:25:22 +0530

    Insert and create a Dependency Injection service collection file.

--- backend/src/NJS.Application/Extensions/ServiceCollectionExtensions.cs
+++ backend/src/NJS.Application/Extensions/ServiceCollectionExtensions.cs
@@ -57,6 +57,7 @@ namespace NJS.Application.Extensions
             services.AddScoped<ITenantRepository, TenantRepository>();
 
             services.AddScoped<ICreateAccountRepository, CreateAccountRepository>();
+            services.AddScoped<ICashflowRepository, CashflowRepository>();
 
            // services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<IAuthService, EnhancedAuthService>();
