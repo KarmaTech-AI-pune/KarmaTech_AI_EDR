16350f813b47ff12554709469d2a53955348d14a
Koteswararao-Peddi7 <koteswararaopeddi850@gmail.com>
2025-12-24 11:12:46 +0530

    Created basic program page

---
+++ frontend/src/features/program/pages/Program.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Program: React.FC = () => {
+  return (
+    <div>
+      <h1>Program Management</h1>
+      <p>This is the Program Management page.</p>
+    </div>
+  );
+};
+
+export default Program;
