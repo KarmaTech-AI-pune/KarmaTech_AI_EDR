2b82970773640d990f1ac1a44c61990d32238dec
Koteswararao-Peddi7 <koteswararaopeddi850@gmail.com>
2025-12-24 11:10:39 +0530

    Written program services and interfaces

---
+++ frontend/src/features/program/types/types.ts
@@ -0,0 +1,10 @@
+export interface Program {
+    id: number;
+    tenantId: number;
+    name: string;
+    description: string;
+    startDate: string; // Assuming ISO 8601 string format for dates
+    endDate: string;   // Assuming ISO 8601 string format for dates
+    createdBy: string;
+    lastModifiedBy?: string;
+}
