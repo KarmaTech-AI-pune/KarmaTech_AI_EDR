46549da1ec801b24a371e64fd4ef6bc3471234b0
Cloud-Enthusiast <shekhareagle@gmail.com>
2026-03-12 15:57:05 +0530

    feat: Add WBS version history migration

---
+++ backend/src/EDR.Domain/Migrations/20260312102403_wbs-version-history-migration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EDR.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class wbsversionhistorymigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentId",
+                table: "WBSTaskVersionHistories",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WBSOptionId",
+                table: "WBSTaskVersionHistories",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "WBSTaskVersionHistories");
+
+            migrationBuilder.DropColumn(
+                name: "WBSOptionId",
+                table: "WBSTaskVersionHistories");
+        }
+    }
+}
