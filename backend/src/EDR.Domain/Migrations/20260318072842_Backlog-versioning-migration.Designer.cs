34df2916ef04c576e72d50be6c20d7d2a5a11d87
Cloud-Enthusiast <shekhareagle@gmail.com>
2026-03-18 13:02:13 +0530

    Added the backlog-versioning migration

--- backend/src/EDR.Domain/Migrations/20260318072842_Backlog-versioning-migration.Designer.cs
+++ backend/src/EDR.Domain/Migrations/20260318072842_Backlog-versioning-migration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EDR.Domain.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EDR.Domain.Migrations
 {
     [DbContext(typeof(ProjectManagementContext))]
-    partial class ProjectManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20260318072842_Backlog-versioning-migration")]
+    partial class Backlogversioningmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
